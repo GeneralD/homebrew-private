e1b9733d09a1c4e92501657b92f520802f25f545
GeneralD <yumejustice@gmail.com>
2017-08-20 17:27:49 +0900

    add consulo app

---
+++ Casks/consulo.rb
@@ -0,0 +1,20 @@
+cask 'consulo' do
+  version :latest
+  sha256 :no_check
+    
+  url 'https://hub.consulo.io/api/repository/download?channel=release&platformVersion=SNAPSHOT&id=consulo-mac64'
+  name 'Consulo'
+  homepage 'https://consulo.io'
+
+  app 'Consulo.app'
+  
+  zap delete: [
+                '~/Library/Application Support/Consulo',
+                '~/Library/Caches/Consulo',
+                '~/Library/Logs/Consulo'
+              ],
+      trash: [
+                '~/Library/Preferences/Consulo'
+      ]
+  
+end
