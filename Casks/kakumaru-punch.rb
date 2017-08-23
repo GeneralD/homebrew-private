a0ec84dfcb11fe8d5e94df3193ee8d455e7a6bdf
GeneralD <yumejustice@gmail.com>
2017-08-23 11:06:06 +0900

    add Kakumaru Punch app

---
+++ Casks/kakumaru-punch.rb
@@ -0,0 +1,10 @@
+cask 'kakumaru-punch' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://sheepapp.com/api/download.php?f=Kakumaru'
+  name 'Kakumaru Punch'
+  homepage 'http://sheepapp.com/app/Kakumaru/'
+
+  app 'Kakumaru Punch.app'
+end
