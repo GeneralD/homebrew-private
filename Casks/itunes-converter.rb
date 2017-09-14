952e6544f63d0c09927b01edbb61b1a624a8a1f6
GeneralD <yumejustice@gmail.com>
2017-09-14 22:49:56 +0900

    modify itunes-converter

--- Casks/itunes-converter.rb
+++ Casks/itunes-converter.rb
@@ -7,4 +7,12 @@ cask 'itunes-converter' do
   homepage 'http://www.macsome.com/itunes-converter-mac'
 
   app 'iTunes Converter.app'
+  
+  zap delete: [
+    '~/Library/Application Support/iTunes Converter',
+    '~/Library/Caches/com.macsome.iTunesConverter'
+  ],
+  trash: [
+    '~/Library/Preferences/com.macsome.iTunesConverter.plist'
+  ]
 end
