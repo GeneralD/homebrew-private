809c196137dce5dcd207728b72b789cab9b2b7b7
GeneralD <yumejustice@gmail.com>
2017-09-27 12:38:07 +0900

    fix unity-package-unpacker

--- Casks/unity-package-unpacker.rb
+++ Casks/unity-package-unpacker.rb
@@ -2,7 +2,7 @@ cask 'unity-package-unpacker' do
   version :latest
   sha256 :no_check
 
-  url 'https://github.com/GeneralD/Unity-Package-Unpacker/archive/master.zip'
+  url 'https://codeload.github.com/GeneralD/Unity-Package-Unpacker/zip/master'
   name 'Unity Package Unpacker'
   homepage 'https://github.com/GeneralD/Unity-Package-Unpacker'
 
