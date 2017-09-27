b917d296df08d98a079d155d747f7bb07c1db742
GeneralD <yumejustice@gmail.com>
2017-09-27 13:11:22 +0900

    add unity-version-selector

---
+++ Casks/unity-version-selector.rb
@@ -0,0 +1,12 @@
+cask 'unity-version-selector' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://codeload.github.com/GeneralD/Unity-Version-Selector/zip/master'
+  name 'Unity Version Selector'
+  homepage 'https://github.com/GeneralD/Unity-Version-Selector'
+  depends_on brew: 'coreutils'
+  
+  app ['Unity-Version-Selector-master/Unity Version Selector.app',
+       'Unity-Version-Selector-master/Unity Version Selector 2017.app']
+end
