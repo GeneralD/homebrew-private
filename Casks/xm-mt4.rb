ca695a1a0826420cbe4ceb1900b0c50ca762d8a9
GeneralD <yumejustice@gmail.com>
2018-01-06 18:19:18 +0900

    [from now] 2018/01/06 18:19:18
    diff --git a/Casks/xm-mt4.rb b/Casks/xm-mt4.rb
    new file mode 100644
    index 0000000..25b8863
    --- /dev/null
    +++ b/Casks/xm-mt4.rb
    @@ -0,0 +1,10 @@
    +cask 'xm-mt4' do
    +  version :latest
    +  sha256 :no_check
    +
    +  url 'https://www.xm.com/download/xm-macmt4'
    +  name 'XM MT4'
    +  homepage 'https://www.xm.com/metatrader-mac'
    +
    +  app 'XM MT4.app'
    +end
    \ No newline at end of file

---
+++ Casks/xm-mt4.rb
@@ -0,0 +1,10 @@
+cask 'xm-mt4' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://www.xm.com/download/xm-macmt4'
+  name 'XM MT4'
+  homepage 'https://www.xm.com/metatrader-mac'
+
+  app 'XM MT4.app'
+end
