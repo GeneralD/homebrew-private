4d8560924c9a99df1c7692edf0e9e6fc63342daa
yumenosuke-k <yumejustice@gmail.com>
2018-03-23 13:09:30 +0900

    add townwifi

---
+++ Casks/townwifi.rb
@@ -0,0 +1,12 @@
+cask 'townwifi' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://storage.googleapis.com/townwifi-downloads/mac/installer/TownWifi.pkg'
+  name 'TownWifi'
+  homepage 'http://townwifi.jp'
+  
+  pkg 'TownWifi.pkg'
+
+  app 'TownWifi.app'
+end
