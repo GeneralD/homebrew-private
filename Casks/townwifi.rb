0d99372508fbbe701027e9e1524abf85028aaa8b
yumenosuke-k <yumejustice@gmail.com>
2018-03-24 13:11:04 +0900

    [from now] 2018/03/24 13:11:04
    diff --git a/Casks/townwifi.rb b/Casks/townwifi.rb
    index 725851e..8e69999 100644
    --- a/Casks/townwifi.rb
    +++ b/Casks/townwifi.rb
    @@ -5,16 +5,17 @@ cask 'townwifi' do
       url 'https://storage.googleapis.com/townwifi-downloads/mac/installer/TownWifi.pkg'
       name 'TownWifi'
       homepage 'http://townwifi.jp'
    -
    +
       pkg 'TownWifi.pkg'
    
       uninstall pkgutil: 'jp.townwifi.townwifi'
    
       zap delete: [
         '~/Library/Application Support/jp.townwifi.townwifi',
    -    '~/Library/Caches/jp.townwifi.townwifi'
    +    '~/Library/Caches/jp.townwifi.townwifi',
         '~/Library/Preferences/com.company.appname.plist'
    -  ],
    +  ]
    +
       trash: [
         '~/Library/Preferences/jp.townwifi.townwifi.plist'
       ]

--- Casks/townwifi.rb
+++ Casks/townwifi.rb
@@ -5,16 +5,17 @@ cask 'townwifi' do
   url 'https://storage.googleapis.com/townwifi-downloads/mac/installer/TownWifi.pkg'
   name 'TownWifi'
   homepage 'http://townwifi.jp'
-  
+
   pkg 'TownWifi.pkg'
 
   uninstall pkgutil: 'jp.townwifi.townwifi'
 
   zap delete: [
     '~/Library/Application Support/jp.townwifi.townwifi',
-    '~/Library/Caches/jp.townwifi.townwifi'
+    '~/Library/Caches/jp.townwifi.townwifi',
     '~/Library/Preferences/com.company.appname.plist'
-  ],
+  ]
+
   trash: [
     '~/Library/Preferences/jp.townwifi.townwifi.plist'
   ]
