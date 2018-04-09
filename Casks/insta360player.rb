946f1209e39ac5cbb82edd48b64fb015bdb8770e
yumenosuke-k <yumejustice@gmail.com>
2018-04-09 17:24:16 +0900

    [from now] 2018/04/09 17:24:16
    diff --git a/Casks/360-video-player.rb b/Casks/insta360player.rb
    similarity index 88%
    rename from Casks/360-video-player.rb
    rename to Casks/insta360player.rb
    index d5b04e3..59dbd7c 100644
    --- a/Casks/360-video-player.rb
    +++ b/Casks/insta360player.rb
    @@ -1,9 +1,9 @@
    -cask '360-video-player' do
    +cask 'insta360player' do
       version '2.3.6'
       sha256 :no_check
    
       url 'https://static.insta360.com/software/Player/Mac/Insta360Player_Mac_v#{version}.pkg'
    -  name 'Insta360 Player'
    +  name 'Insta360Player'
       homepage 'https://www.insta360.com/'
    
       pkg 'Insta360Player_Mac_#{version}.pkg'
    diff --git a/Casks/360-video-studio.rb b/Casks/insta360studio.rb
    similarity index 88%
    rename from Casks/360-video-studio.rb
    rename to Casks/insta360studio.rb
    index 8509399..ef9e3c9 100644
    --- a/Casks/360-video-studio.rb
    +++ b/Casks/insta360studio.rb
    @@ -1,9 +1,9 @@
    -cask '360-video-studio' do
    +cask 'insta360studio' do
       version '2.12.1'
       sha256 :no_check
    
       url 'https://static.insta360.com/software/Studio/Mac/Insta360Studio_Mac_#{version}.pkg'
    -  name 'Insta360 Studio'
    +  name 'Insta360Studio'
       homepage 'https://www.insta360.com/'
    
       pkg 'Insta360Studio_Mac_#{version}.pkg'

--- Casks/insta360player.rb
+++ Casks/insta360player.rb
@@ -1,9 +1,9 @@
-cask '360-video-player' do
+cask 'insta360player' do
   version '2.3.6'
   sha256 :no_check
 
   url 'https://static.insta360.com/software/Player/Mac/Insta360Player_Mac_v#{version}.pkg'
-  name 'Insta360 Player'
+  name 'Insta360Player'
   homepage 'https://www.insta360.com/'
 
   pkg 'Insta360Player_Mac_#{version}.pkg'
--- Casks/insta360studio.rb
+++ Casks/insta360studio.rb
@@ -1,9 +1,9 @@
-cask '360-video-studio' do
+cask 'insta360studio' do
   version '2.12.1'
   sha256 :no_check
 
   url 'https://static.insta360.com/software/Studio/Mac/Insta360Studio_Mac_#{version}.pkg'
-  name 'Insta360 Studio'
+  name 'Insta360Studio'
   homepage 'https://www.insta360.com/'
 
   pkg 'Insta360Studio_Mac_#{version}.pkg'
