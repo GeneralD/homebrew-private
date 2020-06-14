9c527dc1437bbf20c75bc01f17a041ca2dcb74da
GeneralD <yumejustice@gmail.com>
2020-06-14 19:03:30 +0900

    add hhkb-keymap-tool.rb

--- Casks/hhkb-keymap-tool.rb
+++ Casks/hhkb-keymap-tool.rb
@@ -6,7 +6,15 @@ cask 'hhkb-keymap-tool' do
   name 'Happy Hacking Keyboard Keymap Tool'
   homepage 'https://happyhackingkb.com/download'
 
+  depends_on macos: '>= :sierra'
+
   pkg "HHKBkeymapTool_#{version}ma.pkg"
+
+  install delete: '/Applications/HHKB'
+
+  uninstall quit: 'jp.co.pfu.hhkb-keymap-tool',
+            pkgutil: 'jp.co.pfu.hhkb-keymap-tool.pkg.*',
+            delete: '/Applications/hhkb-keymap-tool.app'
 
   zap delete: [
     '~/Library/Application Support/hhkb-keymap-tool'
