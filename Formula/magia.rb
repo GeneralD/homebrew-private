e8ecf2bd1eb5f16f087e2c5c33db913f1e27dc7a
GeneralD <yumejustice@gmail.com>
2023-04-24 23:53:57 +0900

    feat: upgrade magia to 1.0.7

--- Formula/magia.rb
+++ Formula/magia.rb
@@ -1,8 +1,8 @@
 class Magia < Formula
   desc "An ultimate NFT generator for the proffesionals"
   homepage "https://github.com/GeneralD/Magia"
-  url "https://github.com/GeneralD/Magia/archive/refs/tags/1.0.6.tar.gz"
-  sha256 "d98c8bbd52c73c527f74662765378dd31b0a7d9ebe8a829876ed663ce22e5c34"
+  url "https://github.com/GeneralD/Magia/archive/refs/tags/1.0.7.tar.gz"
+  sha256 "ac08836048f06651eb240d9c49405651dc8d1a425da7f97a9a8b69f422b1a730"
 
   option "without-completions", "Disable zsh completions"
 
