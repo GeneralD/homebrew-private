153930ecfa327f6aee83c2a7facbd7e1d36d2756
GeneralD <yumejustice@gmail.com>
2023-11-06 16:00:45 +0900

    [from now] 2023/11/06 16:00:45
    diff --git a/Casks/snap-camera.rb b/Casks/snap-camera.rb
    new file mode 100644
    index 0000000..e09ac21
    --- /dev/null
    +++ b/Casks/snap-camera.rb
    @@ -0,0 +1,34 @@
    +cask 'snap-camera' do
    +  version '1.21.0'
    +  sha256 '0b97133d09d260318b92f7bb097e6c9185e00bc66ad997a228ae4f03ad47498c'
    +
    +  url "https://www.dropbox.com/scl/fi/1spgifeqb6utjnrtz0uid/Snap-Camera-1.21.0.pkg?rlkey=1vl30recl1dxjouoexe2476xp&dl=1"
    +
    +  name 'Snap Camera'
    +  homepage 'https://help.snapchat.com/hc/en-us/articles/7012304217876-Snap-Camera'
    +
    +  pkg "Snap Camera #{version}.pkg"
    +
    +  uninstall pkgutil: 'com.snap.SnapCamera',
    +            quit:    'com.snap.SnapCamera',
    +            signal:  [
    +                        ['KILL', 'com.snap.SnapCamera'],
    +                    ],
    +            delete:  [
    +                        '/Applications/Snap Camera.app',
    +                        '/Library/CoreMediaIO/Plug-Ins/DAL/SnapCamera.plugin',
    +                    ],
    +            launchctl: [
    +                        'com.snap.AssistantService',
    +                        'com.snap.SnapCameraRemover',
    +                        'com.snap.SnapCameraAutoLaunch',
    +                    ]
    +  zap trash: [
    +    '~/Library/Application Support/Snap Camera',
    +    '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.snap.snapcamera.sfl*',
    +     '~/Library/Caches/Snap/Snap Camera',
    +     '~/Library/Preferences/Snap/Snap Camera',
    +     '~/Library/Preferences/com.snap.SnapCamera.plist',
    +     '~/Library/Saved Application State/com.snap.SnapCamera.savedState',
    +    ]
    +end
    \ No newline at end of file

---
+++ Casks/snap-camera.rb
@@ -0,0 +1,34 @@
+cask 'snap-camera' do
+  version '1.21.0'
+  sha256 '0b97133d09d260318b92f7bb097e6c9185e00bc66ad997a228ae4f03ad47498c'
+
+  url "https://www.dropbox.com/scl/fi/1spgifeqb6utjnrtz0uid/Snap-Camera-1.21.0.pkg?rlkey=1vl30recl1dxjouoexe2476xp&dl=1"
+
+  name 'Snap Camera'
+  homepage 'https://help.snapchat.com/hc/en-us/articles/7012304217876-Snap-Camera'
+
+  pkg "Snap Camera #{version}.pkg"
+
+  uninstall pkgutil: 'com.snap.SnapCamera',
+            quit:    'com.snap.SnapCamera',
+            signal:  [
+                        ['KILL', 'com.snap.SnapCamera'],
+                    ],
+            delete:  [
+                        '/Applications/Snap Camera.app',
+                        '/Library/CoreMediaIO/Plug-Ins/DAL/SnapCamera.plugin',
+                    ],
+            launchctl: [
+                        'com.snap.AssistantService',
+                        'com.snap.SnapCameraRemover',
+                        'com.snap.SnapCameraAutoLaunch',
+                    ]
+  zap trash: [
+    '~/Library/Application Support/Snap Camera',
+    '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.snap.snapcamera.sfl*',
+     '~/Library/Caches/Snap/Snap Camera',
+     '~/Library/Preferences/Snap/Snap Camera',
+     '~/Library/Preferences/com.snap.SnapCamera.plist',
+     '~/Library/Saved Application State/com.snap.SnapCamera.savedState',
+    ]
+end
