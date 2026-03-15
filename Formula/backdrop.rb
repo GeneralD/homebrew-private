9841508b768feb0092b3cee61228147550c87663
YUMENOSUKE <yumejustice@gmail.com>
2026-03-15 20:51:33 +0900

    Use libexec for binary + bundles, env_script wrapper in bin

--- Formula/backdrop.rb
+++ Formula/backdrop.rb
@@ -5,19 +5,26 @@ class Backdrop < Formula
   sha256 "5880f0935c83f3b5171bd46f6a0562848a0179e2ba3467f75939098f948f52ec"
 
   def install
-    system "make", "install", "PREFIX=#{prefix}"
+    system "swift", "build", "--disable-sandbox", "-c", "release"
 
-    output = Utils.safe_popen_read(bin/"backdrop", "completion", "zsh")
+    build_dir = Utils.safe_popen_read("swift", "build", "--show-bin-path", "-c", "release").strip
+
+    libexec.install Dir["#{build_dir}/backdrop"]
+    libexec.install Dir["#{build_dir}/*.bundle"]
+
+    (bin/"backdrop").write_env_script libexec/"backdrop", {}
+
+    output = Utils.safe_popen_read(libexec/"backdrop", "completion", "zsh")
     (zsh_completion/"_backdrop").write output
 
-    output = Utils.safe_popen_read(bin/"backdrop", "completion", "bash")
+    output = Utils.safe_popen_read(libexec/"backdrop", "completion", "bash")
     (bash_completion/"backdrop").write output
 
-    output = Utils.safe_popen_read(bin/"backdrop", "completion", "fish")
+    output = Utils.safe_popen_read(libexec/"backdrop", "completion", "fish")
     (fish_completion/"backdrop.fish").write output
   end
 
   test do
-    system "#{bin}/backdrop", "version"
+    system libexec/"backdrop", "version"
   end
 end
