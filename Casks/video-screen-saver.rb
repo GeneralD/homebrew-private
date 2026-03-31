9cb993bc973a9a6ef245f396055c8ef3479f597a
YUMENOSUKE <yumejustice@gmail.com>
2026-04-01 00:47:21 +0900

    Update video-screen-saver to 1.0.2

--- Casks/video-screen-saver.rb
+++ Casks/video-screen-saver.rb
@@ -1,8 +1,8 @@
 cask "video-screen-saver" do
-  version "1.0.1"
-  sha256 "aed6a2026464cfa29e0f9ef94aebd891ae3d95221be53587a6062861a3053d49"
+  version "1.0.2"
+  sha256 "f0d089c946acf553e15152fc17dcb0bf4c97bd9765adfdfa993ecb22a96fbf19"
 
-  url "https://github.com/GeneralD/VideoScreenSaver/releases/download/v1.0.1/VideoScreenSaver-1.0.1.zip"
+  url "https://github.com/GeneralD/VideoScreenSaver/releases/download/v1.0.2/VideoScreenSaver-1.0.2.zip"
   name "VideoScreenSaver"
   desc "macOS screensaver that plays a video file in a loop"
   homepage "https://github.com/GeneralD/VideoScreenSaver"
