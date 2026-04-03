bc12c4e05e6e2d4ef7edddeba0de7e6cebdce2e0
YUMENOSUKE <yumejustice@gmail.com>
2026-04-03 20:54:23 +0900

    Fix video-screen-saver SHA-256 checksum for v1.0.2

--- Casks/video-screen-saver.rb
+++ Casks/video-screen-saver.rb
@@ -1,6 +1,6 @@
 cask "video-screen-saver" do
   version "1.0.2"
-  sha256 "f0d089c946acf553e15152fc17dcb0bf4c97bd9765adfdfa993ecb22a96fbf19"
+  sha256 "08c357778464db2f253c8159655645639e9e2d00c44a858969d35616b09636ef"
 
   url "https://github.com/GeneralD/VideoScreenSaver/releases/download/v1.0.2/VideoScreenSaver-1.0.2.zip"
   name "VideoScreenSaver"
