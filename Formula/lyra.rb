4403ea9cb898cee939895bd9b1109ea8d0a4322d
YUMENOSUKE <yumejustice@gmail.com>
2026-04-28 10:51:31 +0900

    Update lyra to 2.9.1

--- Formula/lyra.rb
+++ Formula/lyra.rb
@@ -1,9 +1,9 @@
 class Lyra < Formula
   desc "Desktop lyrics overlay and video wallpaper for macOS"
   homepage "https://github.com/GeneralD/lyra"
-  url "https://github.com/GeneralD/lyra/releases/download/v2.8.2/lyra-2.8.2-macos-arm64.tar.gz"
-  version "2.8.2"
-  sha256 "674a707cd36dbef4b88611b8b16235fd084b42ee44eda166bbbb1356320ceaf2"
+  url "https://github.com/GeneralD/lyra/releases/download/v2.9.1/lyra-2.9.1-macos-arm64.tar.gz"
+  version "2.9.1"
+  sha256 "9a4772b88f7d73ce7ef6df50cc53fd38b44c83d23f7aedfe3bbb4cccae24d794"
 
   depends_on :macos
   depends_on "yt-dlp"
